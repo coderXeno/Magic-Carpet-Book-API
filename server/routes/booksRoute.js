88a49bbef03a54f6db98ee864d38ebb5a2b1aeab
coderXeno <h.37.diptangsuGupta@gmail.com>
2023-03-19 12:29:19 +0530

    book api done

--- server/routes/booksRoute.js
+++ server/routes/booksRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const BookModel = require("../models/BookModel");
 const AuthModel = require("../models/authModel");
 const bookModule = require("../books");
+const uuid = require("uuid");
 
 router.get('/get-all-books/:userId', async (req, res) => {
     const userId = req.params.userId;
@@ -58,7 +59,8 @@ router.get('/get-book-details/:userId/:bookId', async (req, res) => {
             const userData = await AuthModel.findOne({ userId: reviewObject.userId });
             bookReviews.push({
                 ...reviewObject,
-                authorName: userData.userName
+                authorName: userData.userName,
+                userId: undefined
             });
         }
 
@@ -94,6 +96,7 @@ router.post('/add-review/', async (req, res) => {
     }
 
     let reviewObject = {
+        "reviewId": uuid.v4(),
         "userId": userId,
         "message": message,
         "createdAt": new Date().toJSON()
@@ -109,6 +112,49 @@ router.post('/add-review/', async (req, res) => {
         return res.status(200).send({
             success: true,
             message: "Successfully added review"
+        });
+    }
+});
+
+router.post('/edit-review/', async (req, res) => {
+    const { userId, bookId, reviewId, newMessage } = req.body;
+
+    const userData = await AuthModel.findOne({ userId: userId });
+    if(userData === null){
+        return res.status(404).send({
+            success: false,
+            message: "Please log in to add review!"
+        });
+    }
+
+    const bookData = await BookModel.findOne({ bookId: bookId });
+    if(bookData === null){
+        return res.status(404).send({
+            success: false,
+            message: "No such book found!"
+        });   
+    }
+
+    const editedReview = await BookModel.updateOne(
+        { 
+            bookId: bookId,
+            "bookReviews.reviewId": reviewId, 
+            "bookReviews.userId": userId
+        },
+        {
+           $set: { "bookReviews.$.message": newMessage }
+        }
+    );
+
+    if(editedReview){
+        return res.status(200).send({
+            success: true,
+            message: "Successfully edited review!"
+        });
+    } else {
+        return res.status(404).send({
+            success: true,
+            message: "Review not found!"
         });
     }
 });
